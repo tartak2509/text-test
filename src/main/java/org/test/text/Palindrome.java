c3dcbb6a7ece97533facc42c6cae0e1e46095ef6
Taras Shpulyar <shpulyar.25@gmail.com>
2016-12-22 15:24:19 +0200

    create simple project structure

---
+++ src/main/java/org/test/text/Palindrome.java
@@ -0,0 +1,16 @@
+package org.test.text;
+
+/**
+ * Write an efficient algorithm to check if a string is a palindrome. A string is a palindrome if the string matches the reverse of string. Example: 1221 is a palindrome but not 1121.
+ * 
+ * @author tartak25
+ *
+ */
+public class Palindrome {
+
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/org/test/text/WordsCounter.java
@@ -0,0 +1,16 @@
+package org.test.text;
+
+/**
+ * Create a command-line Java program that counts unique words from a text file and lists the top N occurrences. Words are separated by space.
+ *
+ * @author tartak25
+ *
+ */
+public class WordsCounter {
+
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

7ddda4b9f1178da5ba6d9e3be1b90ff0f1b8167f
Taras Shpulyar <shpulyar.25@gmail.com>
2016-12-23 15:59:38 +0200

    added String splitter to test

--- src/main/java/org/test/text/TokenizerVsSpliter.java
+++ src/main/java/org/test/text/TokenizerVsSpliter.java
@@ -19,11 +19,11 @@ public class TokenizerVsSpliter {
 		final String filepath = Thread.currentThread().getContextClassLoader().getResource(WordsCounter.DEFAULT_FILENAME).getPath();
 		try (final Stream<String> stream = Files.lines(Paths.get(filepath))) {
 			final String testStr = stream.reduce("", String::concat);
-			final int[] amounts = new int[] { 100, 1000, 10_000, 100_000 };
+			final int[] amounts = new int[] { 100, 1000, 10_000, 50_000, 100_000 };
 			Arrays.stream(amounts).forEach(amount -> {
 				final long start = System.currentTimeMillis();
 				LOGGER.info("Testing on amount {}", amount);
-				listWinner(tokenizerTime(testStr, amount), spliteratorTime(testStr, amount));
+				listWinner(tokenizerTime(testStr, amount), apacheSpliteratorTime(testStr, amount), spliteratorTime(testStr, amount));
 				LOGGER.log(Level.WARN, "Perfomance test on ammount {} time: -> {}", amount, System.currentTimeMillis() - start);
 			});
 		}
@@ -48,12 +48,28 @@ public class TokenizerVsSpliter {
 		return tokinizerTime;
 	}
 
-	private static long spliteratorTime(final String content, final long loopSize) {
+	private static long apacheSpliteratorTime(final String content, final long loopSize) {
 		long spliteratorTime = System.currentTimeMillis();
 		long start;
 		for (long i = 0; i < loopSize; i++) {
 			start = System.currentTimeMillis();
 			final String[] splited = StringUtils.split(content.concat(Long.toString(i)), WordsCounter.WORDS_DELIMITER);
+			LOGGER.log(Level.DEBUG, "Apache spliterator time -> {}", System.currentTimeMillis() - start);
+			start = System.currentTimeMillis();
+			Arrays.stream(splited).forEach(str -> {
+			});
+			LOGGER.log(Level.DEBUG, "Apache spliterator loop time -> {}", System.currentTimeMillis() - start);
+		}
+		spliteratorTime = System.currentTimeMillis() - spliteratorTime;
+		LOGGER.log(Level.INFO, "Apache spliterator total time -> {}", spliteratorTime);
+		return spliteratorTime;
+	}
+	private static long spliteratorTime(final String content, final long loopSize) {
+		long spliteratorTime = System.currentTimeMillis();
+		long start;
+		for (long i = 0; i < loopSize; i++) {
+			start = System.currentTimeMillis();
+			final String[] splited = content.concat(Long.toString(i)).split(WordsCounter.WORDS_DELIMITER);
 			LOGGER.log(Level.DEBUG, "spliterator time -> {}", System.currentTimeMillis() - start);
 			start = System.currentTimeMillis();
 			Arrays.stream(splited).forEach(str -> {
@@ -65,11 +81,13 @@ public class TokenizerVsSpliter {
 		return spliteratorTime;
 	}
 
-	private static void listWinner(final long tokenizerTime, final long spliteratorTime) {
-		if (spliteratorTime < tokenizerTime) {
+	private static void listWinner(final long tokenizerTime, final long apacheSpliteratorTime, final long spliteratorTime) {
+		if (spliteratorTime < tokenizerTime && spliteratorTime < apacheSpliteratorTime) {
 			LOGGER.info("Spliterator wins !!!");
-		} else {
+		} else if (tokenizerTime < apacheSpliteratorTime) {
 			LOGGER.info("Tokenizer wins !!!");
+		} else {
+			LOGGER.info("Apache spliterator wins !!!");
 		}
 	}
 }
